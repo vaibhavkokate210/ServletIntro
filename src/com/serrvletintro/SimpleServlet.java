d9d579b82c39b46105156dfc542cd8348485ed5f
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-10-01 20:48:19 +0530

    [Vaibhav] added first servlet is created

---
+++ src/com/serrvletintro/SimpleServlet.java
@@ -0,0 +1,22 @@
+package com.serrvletintro;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Hello")
+public class SimpleServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<h3>Hello world Vaibhav typing again</h3>");
+		out.close();
+	}
+	
+}
